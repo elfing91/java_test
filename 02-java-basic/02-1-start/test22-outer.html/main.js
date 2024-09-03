b37281edb44fa25f8a7a6e6b0863c82c61033282
민정 <minjaung5073@naver.com>
2024-09-03 14:25:44 +0900

    자바스크립트 코드를 내부/외부에서 각각 구현하기

---
+++ 02-java-basic/02-1-start/test22-outer.html/main.js
@@ -0,0 +1,9 @@
+const myButtonClick=()=>{
+    let title=document.getElementById('title')
+    if(title.style.backgroundColor=='yellow'){
+        title.style.backgroundColor='pink'
+    }else{
+        title.style.backgroundColor='yellow'
+
+    }
+}
