b83613e6aaa89c2d15defb52fa44dd7fdb9889ba
민정 <minjaung5073@naver.com>
2024-09-05 15:26:46 +0900

    array 삽입/삭제 설정

---
+++ 02-java-basic/02-2-variable-type/test6-undefined-null/main.js
@@ -0,0 +1,17 @@
+let user1={
+    name: '홍길동',
+    age:25,
+    adress:'서울',
+}
+
+let user2=null
+console.log(typeof user1, typeof user2) //object object
+
+
+let data2
+console.log(data2, typeof user2) //undefined 'undefined'
+
+
+
+
+
