5ee197f6c9fae1b1abda54b13ce010e695c5bfb2
민정 <minjaung5073@naver.com>
2024-09-10 17:11:36 +0900

    입력수 미만의 소수찾기문제-isprim=true이용해 해결

--- 02-java-basic/test11-quiz1/main.js
+++ 02-java-basic/test11-quiz1/main.js
@@ -1,29 +1,41 @@
 "use strict";
 
 const primeTest = () => {
-    let no = prompt('2 이상의 숫자를 입력하세요')
-    var message = ''
+    let no = prompt("2 이상의 숫자를 입력하세요");
+    
+
+    if (no < 2) {
+        document.querySelector("#result").innerHTML = "2 이상의 숫자를 입력하세요!";
+        return;
+    }
+
+    let array = [];
+
+    //어떤 수가 소수인지를 판단, 2부터 증가시셔서 나누어야 한다. ==> loop
+    //2부터 입력받은 수까지 증가시키면서 소수인지를 판단한다. ==>loop
+
     let result
 
-
-    for (let i = 2; i < no; i++) {
-        result = no % i
-        if (result === 0) {
-            break
+    for (let j = 2; j <= no; j++) {
+        let isPrime = true;
+        
+        for (let i = 2; i < j; i++) {
+            result = j % i
+            if (result === 0) {
+                 isPrime = false;
+                break
+            }
+                 
         }
-    }
+        if(isPrime){
+            array.push(j);
+        }
 
-    if (result == 0 || no <= 1) {
-        message = `${no}는 소수가 아닙니다`
-
-    } else {
-        message = `${no}는 소수입니다`
     }
 
 
 
+    let message=`입력하신 ${no}까지의 소수는 ${array.join(', ')}입니다`
 
-
-    //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
-    document.querySelector('#result').innerHTML = message
-}
+    document.querySelector("#result").innerHTML = message;
+};
