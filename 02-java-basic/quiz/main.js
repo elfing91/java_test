b8be7b5618177b1391b57902c926778025f9e061
민정 <minjaung5073@naver.com>
2024-09-10 11:11:37 +0900

    for문 구구단을 while문으로 구현

--- 02-java-basic/quiz/main.js
+++ 02-java-basic/quiz/main.js
@@ -1,11 +1,15 @@
 "use strict"
 
-
-for(let i=1;i<=9;i++){
+let i=1;
+while(i<=9){
     document.write(`<div><h3>${i}단</h3>`);
-    for(let j=1;j<=9;j++){
+    let j=1
+    while(j<=9){
+        
         document.write(`${i}*${j}=${i*j}<br>`)
+        j++
     }
+    i++
     document.write(`</div>`);
 }
 //✨js코드에 바로 html마크업을 적용시킬 수 있다
