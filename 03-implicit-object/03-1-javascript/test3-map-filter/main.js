33af875f4687d04f77d712db66d93cee8c84f436
민정 <minjaung5073@naver.com>
2024-09-13 14:25:54 +0900

    모든 배열 데이터에 연산을 적용하는 map함수

--- 03-implicit-object/03-1-javascript/test3-map-filter/main.js
+++ 03-implicit-object/03-1-javascript/test3-map-filter/main.js
@@ -12,4 +12,11 @@ console.log(result)//11,20,15
 result=array.every((value)=>{
     return value>2 //모든 데이터의 값이 4보다 크다면 true
 })
+console.log(result) //2보다 모든값이 크니 true
+
+
+//배열의 데이터에 모두 2를 곱한 결과값-최종결과는 배열
+result=array.map((value)=>{
+    return value*2
+})
 console.log(result)
