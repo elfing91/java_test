a4cf7699e7e86875a72ee0a2cb9ea583ebe63db4
민정 <minjaung5073@naver.com>
2024-09-13 13:42:25 +0900

    데이터 맨뒤에 삽입,삭제하는 함수 push/pop과 맨 앞에 삽입, 삭제하는 unshift/shift함수

--- 03-implicit-object/03-1-javascript/test2-array-function/main.js
+++ 03-implicit-object/03-1-javascript/test2-array-function/main.js
@@ -10,4 +10,19 @@ console.log(array3)
 let result1=array3.join(',')//데이터 사이에 구분자 추가
 console.log(result1)
 
+//배열에 데이터 추가 맨뒤/맨앞
+array3.push(100) //맨뒤에 추가
+array3.unshift(200) //맨앞에 추가
+
+//한번에 여러개 추가가능
+array3.push(1000,2000)
+array3.unshift(3000,4000)
+
+//배열에 데이터 삭제 
+array3.pop()////맨 뒤 삭제
+array3.shift()//맨앞 삭제
+
+
+
+
 
