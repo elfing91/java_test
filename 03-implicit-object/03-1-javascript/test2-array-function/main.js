4aac2ef9a93d56d971bb4beb413da65e7fff6a2f
민정 <minjaung5073@naver.com>
2024-09-13 13:37:40 +0900

    배열 데이터 사이에 구분자 추가하는 join함수

--- 03-implicit-object/03-1-javascript/test2-array-function/main.js
+++ 03-implicit-object/03-1-javascript/test2-array-function/main.js
@@ -3,5 +3,11 @@ let array1=[10,20]
 let array2=[30,40]
 
 //두 배열을 결합
-let array3=array1.concat(array2)
+let array3=array1.concat(array2)//배열 결합
 console.log(array3)
+
+//배열의 모든 데이터를 구분자로 결합해 하나의 문자열로 결합
+let result1=array3.join(',')//데이터 사이에 구분자 추가
+console.log(result1)
+
+
