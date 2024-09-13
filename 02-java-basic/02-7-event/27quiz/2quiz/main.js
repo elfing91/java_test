bd54f979b4a157d4e9801a8ae178a2e1715d51dd
민정 <minjaung5073@naver.com>
2024-09-13 10:26:19 +0900

    id 인풋에 입력받은 idNode값 출력

--- 02-java-basic/02-7-event/27quiz/2quiz/main.js
+++ 02-java-basic/02-7-event/27quiz/2quiz/main.js
@@ -1,7 +1,16 @@
 "use strict"
 
+//valid상황을 여러함수에서 공유해야 한다면 global로 한다
+let isIdValid=false
+let isPwValid=false
+let id=''
+let pw=''
+
+
 function idValidCheck(){
-    alert('id valid')
+    let idNode=document.getElementById('id')
+    id=idNode.value
+    alert(id)//id 인풋에 입력받은 idNode값 출력 
 }
 
 function pwValidCheck(){
