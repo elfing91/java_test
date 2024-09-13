d95b0f774d282ff213593f968687be3ea7739eb8
민정 <minjaung5073@naver.com>
2024-09-13 14:23:34 +0900

    배열의 모든 값이 조건에 만족하는지 출력하는 every함수

--- 03-implicit-object/03-1-javascript/test3-map-filter/main.js
+++ 03-implicit-object/03-1-javascript/test3-map-filter/main.js
@@ -7,3 +7,9 @@ let result=array.filter((value)=>{
 })
 
 console.log(result)//11,20,15
+
+//배열의 데이터가 모두 조건에 만족하는가?
+result=array.every((value)=>{
+    return value>2 //모든 데이터의 값이 4보다 크다면 true
+})
+console.log(result)
