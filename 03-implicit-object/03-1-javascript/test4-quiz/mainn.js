8dc3ff2dba783d281d0dc400909adb055f28e933
민정 <minjaung5073@naver.com>
2024-09-15 23:58:16 +0900

    map함수와 filter함수 완료

--- 03-implicit-object/03-1-javascript/test4-quiz/mainn.js
+++ 03-implicit-object/03-1-javascript/test4-quiz/mainn.js
@@ -1,38 +1,49 @@
 'use strict'
 
-let inputNode=document.getElementById("word")
-let addNode=document.getElementById("addBtn")
-let mapNode=document.getElementById("mapBtn")
-let filterNode=document.getElementById("filterBtn")
-let sortNode=document.getElementById("sortBtn")
-let resultNode=document.getElementById("result")
+let inputNode = document.getElementById("word")
+let addNode = document.getElementById("addBtn")
+let mapNode = document.getElementById("mapBtn")
+let filterNode = document.getElementById("filterBtn")
+let sortNode = document.getElementById("sortBtn")
+let resultNode = document.getElementById("result")
 
-let wordArray=[]
-let resultArray=''
+let wordArray = []
+let resultArray = ''
 
-function printResult(array){
-    array.forEach((index,value) => {
-        resultArray+=`<li>${array}</li>`
-    });
+
+function printResult(array) {//✨array를 매개변수로 전달받음
+    array.forEach((index, value) => { //✨전달받은 결과 배열의 각 요소(인덱스, 값)
+        //array.forEach((index.value)=>{}) //resultArray에 각 배열의 요소마다 입력받는 array값 추가하기
+        resultArray = `<li>${array}</li>`//✨최종출력배열에 li형식으로 할당
+    })
     resultNode.innerHTML(resultArray)
-
 }
-function input(){
-    let word=inputNode.value//인풋노드의 값(id가 word인 엘레먼트의 인풋값)을 word변수에 할당
+
+function add() {
+    let word = inputNode.value//인풋노드의 값(id가 word인 엘레먼트의 인풋값)을 word변수에 할당
     wordArray.push(word)
 
     printResult(wordArray)
 }
 
-function map(){
+function map() {
+    let resultArray = wordArray.map((value) => {
+        return value.toUppercase()
+    })
     printResult(resultArray)
 
 }
-function filter(){
+function filter() {
+    let resultArray=wordArray.filter((value)=>{
+        return value.length()>5
+    })
     printResult(resultArray)
 
 }
-function sort(){
+function sort() {
+    let resultArray=wordArray.sort((value)=>{
+        
+    })
     printResult(resultArray)
 
 }
