58154d6e21360840c12c5a8a321d97e1a6c314df
민정 <minjaung5073@naver.com>
2024-09-19 11:03:32 +0900

    반올림 함수와 랜덤함수

---
+++ 03-implicit-object/test6-math/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//round-반올림
+console.log(Math.round(5.66))//6->5.66반올림
+
+//random- 0~1사이의 난수
+console.log(Math.random())
+
+//random- 0~100사이의 난수
+console.log(Math.random()*100)
+
+
+//22-93사이의 난수
+let max=93
+let min=22
+console.log(Math.random()*(max-min)+min)
+
