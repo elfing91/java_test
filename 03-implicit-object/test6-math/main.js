6e5dccf12b860605db39195f7b0948632a546cba
민정 <minjaung5073@naver.com>
2024-09-19 11:10:20 +0900

    최대,최고값 함수

--- 03-implicit-object/test6-math/main.js
+++ 03-implicit-object/test6-math/main.js
@@ -1,4 +1,5 @@
 "use strict"
+//math객체의 함수들
 
 //round-반올림
 console.log(Math.round(5.66))//6->5.66반올림
@@ -14,4 +15,14 @@ console.log(Math.random()*100)
 let max=93
 let min=22
 console.log(Math.random()*(max-min)+min)
+
+//min/max- 최대,최소함수
+console.log(Math.min(11,22,33,44))//11
+console.log(Math.max(11,22,33,44))//44
+
+//배열에서의 최대,최소값 추출
+let array=[22,44,66,11]
+
+console.log(Math.min(...array))//11
+console.log(Math.max(...array))//66
 
