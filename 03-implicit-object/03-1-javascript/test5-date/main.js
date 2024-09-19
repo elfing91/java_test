148f195141a6cf593354ddb8887b76889f91ccb9
민정 <minjaung5073@naver.com>
2024-09-19 10:18:47 +0900

    date함수로 현재시간과 특정시간 출력

---
+++ 03-implicit-object/03-1-javascript/test5-date/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//현재 시간/날짜
+//new-샛체생성 연산자(모든 객체는 new로 선언해 만든다)
+
+let date1=new Date()
+console.log(date1.toString())//Thu Sep 19 2024 10:14:43 GMT+0900 (한국 표준시)
+console.log(date1.toLocaleString())//2024. 9. 19. 오전 10:14:43
+
+//특정시간을 지정해 객체생성
+let date2=new Date('2024-09-19 10:14:43')
+console.log(date2.toLocaleString())//2024. 9. 19. 오전 10:14:43
+
+let date3=new Date(2024,9-1,19,10,10,10)//월데이터는 0부터시작, 0이 1월
+console.log(date3.toLocaleString())//2024. 10. 19. 오전 10:10:10
+//0에서 웰데이터가 시작하므로 9월이 아니라 10월이 출력->9-1로 월데이터 입력
+
