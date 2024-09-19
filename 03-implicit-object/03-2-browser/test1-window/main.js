8f86a7ebb3db9dfa5e4c39b77403f6bc516496dd
민정 <minjaung5073@naver.com>
2024-09-19 13:05:14 +0900

    WINDOW의 기본 정보 추출

---
+++ 03-implicit-object/03-2-browser/test1-window/main.js
@@ -0,0 +1,6 @@
+"use strict"
+
+//WINDOW의 기본 정보 추출
+console.log(window.innerWidth,window.innerHeight)//875 877
+console.log(window.outerWidth,window.outerHeight)//714 829
+console.log(window.screenLeft,window.screenTop)
