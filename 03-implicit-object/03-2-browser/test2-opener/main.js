ae3764697e182bbef5b188345e6a5a72c4fdcb2d
민정 <minjaung5073@naver.com>
2024-09-20 09:44:57 +0900

    location객체 테스트

--- 03-implicit-object/03-2-browser/test2-opener/main.js
+++ 03-implicit-object/03-2-browser/test2-opener/main.js
@@ -1,3 +1,15 @@
+//child창의 html-js에서 접근할 데이터, 함수
+let mainData=10
+
+function mainFun(){
+    console.log('mainFun 호출')
+}
+
 function goChild(){
     window.open('child.html','_blank','left=100,top=100,width=300,height=400')
+}
+
+function getChildData(){
+    console.log(child.childData)
+    child.childFun()
 }
