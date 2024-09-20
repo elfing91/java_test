c74468d31d31d881996d5e8c3523d2140cb52e24
민정 <minjaung5073@naver.com>
2024-09-20 13:34:47 +0900

    회원가입 퀴즈 시작

---
+++ 03-implicit-object/03-3-document/test2-quiz-selector/main.js
@@ -0,0 +1,14 @@
+let nameNode=document.getElementById('name')
+let hobbyNode=document.getElementsByName('hobby')
+let genderNode=document.getElementsByName('gender')
+let resultNode=document.getElementById('result')
+let formNode=document.getElementById('myForm')
+
+
+function member(){
+
+}
+
+function printResult(){
+    resultNode.innerHTML=msg
+}
