8ee2d37b9889bab9f64af2b4b9064130101b960d
민정 <minjaung5073@naver.com>
2024-09-23 15:13:38 +0900

    객체에서의 this예약어/다양한 객체내 함수선언 방법

--- 04-oop/04-1-literal/teat1-literal/main.js
+++ 04-oop/04-1-literal/teat1-literal/main.js
@@ -15,7 +15,15 @@ let user={
         console.log(`hello,${this.name}`)
     },
 
+    //객체내에서 자신의 다른 멤버(변수나 함수)를 이용하려면 this예약어로 접근해야 한다
+    //this: 객체내에서 자기자신을 지칭하는 예약어
     sayhello1:function(){
+        console.log(`hello,${this.name}-${this.age}`)
+    },
+
+    //객체내에서 함수를 화살표 함수로 선언하는게 문제가 아니라
+    //화살표 함수내에서 this가 객체자신을 지칭하지 못하는게 묹제
+    sayhello2:()=>{
         console.log(`hello,${this.name}-${this.age}`)
     }
 }
@@ -25,7 +33,8 @@ let user={
 console.log(user.name)//user객체중 name변수에 접근
 console.log(user.order.count)//user객체내의 order객체중 count변수에 접근
 user.sayhello()
-user.sayhello1()
+user.sayhello1()//hello,홍길동-20
+user.sayhello2()//hello,-undefined
 
 
 
