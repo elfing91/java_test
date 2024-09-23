dfca91d965dafe7f2320d12fb72ab0455eeba653
민정 <minjaung5073@naver.com>
2024-09-23 10:33:07 +0900

    각 style 출력

---
+++ 03-implicit-object/03-3-document/test5-style/main.js
@@ -0,0 +1,16 @@
+let area1=document.getElementById('area1')
+let area2=document.getElementById('area2')
+let area3=document.getElementById('area3')
+
+//아래 코드는 node.style, 즉 dom node에서 획득한것
+//node트리에 유지되는 css정보는 inline 스타일 뿐이다
+
+console.log(area1.style.width)//200
+console.log(area2.style.width)//
+console.log(area3.style.width)//
+
+//css속성명은 camelCase를 이용하여야 하므로 -가 아니라 대문자를 이용
+//background-color->backgroundColor
+console.log(area1.style.height)
+console.log(area1.style.backgroundColor)
+
