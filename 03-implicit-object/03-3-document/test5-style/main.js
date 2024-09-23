4874f3a768ce4873980a1ec4c259c454ced3aa9c
민정 <minjaung5073@naver.com>
2024-09-23 10:39:17 +0900

    inline스타일이 아닌 속성값 획득-getComputedStyle().속성

--- 03-implicit-object/03-3-document/test5-style/main.js
+++ 03-implicit-object/03-3-document/test5-style/main.js
@@ -5,7 +5,7 @@ let area3=document.getElementById('area3')
 //아래 코드는 node.style, 즉 dom node에서 획득한것
 //node트리에 유지되는 css정보는 inline 스타일 뿐이다
 
-console.log(area1.style.width)//200
+console.log(area1.style.width)//200px
 console.log(area2.style.width)//
 console.log(area3.style.width)//
 
@@ -19,4 +19,10 @@ area1.addEventListener('click',function(){//클릭 이벤트시 노란원으로 
     area1.style.backgroundColor='yellow'
     area1.style.borderRadius='50%'
 })
+
+//inline스타일이 아니라 외부에 선언되어 적용된 css값 획득
+console.log(getComputedStyle(area1).width)//200px
+console.log(getComputedStyle(area2).width)//200px
+console.log(getComputedStyle(area3).width)//200px
+
 
