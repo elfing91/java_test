09ed1a6431ecf9752f4b9f60be3276313076ea92
민정 <minjaung5073@naver.com>
2024-09-24 11:41:19 +0900

    일반함수로 호출시 에러x/ 객체생성으로 호출시 에러발생

--- 04-oop/04-2-constructor-function/test1-basic/main.js
+++ 04-oop/04-2-constructor-function/test1-basic/main.js
@@ -11,4 +11,12 @@ function User1(arg1,arg2){
 }
 //일반 함수로 호출->함수가 일반함수로 준비되어있으므로 문제없음
 User1('홍길동',20)
+
 //객체생성으로 호출
+let obj1=new User1('홍길동',30)
+console.log(`${obj1.name},${obj1.age}`)//undefined,undefined
+//new를 사용했으므로 this는 준비됨(메모리 준비됨)
+//그러나 함수내에 this내에는 아무것도 추가되지 않음->빈 상태의 객체
+//객체내에는 name,age,sayHello()가 없다
+
+//생성자 함수로 준비
