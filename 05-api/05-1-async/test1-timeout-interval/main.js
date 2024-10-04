98fed072bc5592c4f0a503376db187b7359d7f2c
민정 <minjaung5073@naver.com>
2024-10-04 10:07:56 +0900

    async로 함수선언

--- 05-api/05-1-async/test1-timeout-interval/main.js
+++ 05-api/05-1-async/test1-timeout-interval/main.js
@@ -12,3 +12,13 @@ console.time()//timeEnd에서 걸린시간 출력
 //1초뒤에 실행
 setTimeout(sayHello,1000) //default: 1004.243896484375 ms
 
+
+//데이터 전달-함수는 직접 실행시키는 게 아니라 의뢰하는 것이므로
+//의뢰내용에 데이터를 전달해야 한다
+function sayHello2(arg1,arg2,arg3){
+    console.log(`Hello${arg1},${arg2},${arg3}`)
+}
+setTimeout(sayHello2,1000,'김씨',10,true) //Hello김씨,10,true
+//매개변수 전달하기
+
+
---
+++ 05-api/05-1-async/test5-async-wait/main.js
@@ -0,0 +1,21 @@
+//test1-async로 함수선언
+//비동기적으로 실행될 함수-호출한 곳이 대기하지 않도록 하는 함수
+async function  myFun1() {
+    setTimeout(()=>console.log('myFun1 call'),2000)
+    
+}
+let  myFun2=async()=> {
+    setTimeout(()=>console.log('myFun2 call'),1000)
+    
+}
+console.log('step1')
+myFun1()
+console.log('step2')
+myFun2()
+console.log('step3')
+
+// step1
+// step2
+// step3
+// myFun2 call
+// myFun1 call
